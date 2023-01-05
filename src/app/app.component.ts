74e9d55af3b04cf4a02dc1dbdc32cc76e1c3e744
Your Name <you@example.com>
2023-01-05 20:08:31 +0100

    peticion get usuarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'SaraBR';
-}
 
+
+export class AppComponent {
+
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.makeApiRequest();
+  }
+  makeApiRequest() {
+    this.http.get('localhost:8082/api/usuarios').subscribe(
+      (response) => {
+        console.log("lo que devuelve de usuarios: " + response);
+        // procesar la respuesta de la API
+      },
+      (error) => {
+        console.log("error: " + error);
+      }
+    );
+}
+}
