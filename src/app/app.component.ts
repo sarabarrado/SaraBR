2a9f9309898d74b5c060b7e29cb3f627bca438fb
Your Name <you@example.com>
2023-01-09 19:08:55 +0100

    tabla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,25 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { registerLocaleData } from '@angular/common';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 
 @Component({
   selector: 'app-root',
@@ -12,6 +31,8 @@ import { registerLocaleData } from '@angular/common';
 
 export class AppComponent {
   data: any;
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
   constructor(private http: HttpClient) {}
   ngOnInit() {
     this.makeApiResponse();
@@ -29,12 +50,14 @@ export class AppComponent {
       }
     );
 }
+
+
 makeApiResponse(){
   const registro={
     name: 'Luis',
     mail:'luis@gmail.com',
     password:'12345',
-    libro_id:'7'
+    libro_id:7
 
   }
   this.http.post('http://localhost:8082/api/usuarios',registro).subscribe(
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +15,12 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatTableModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
