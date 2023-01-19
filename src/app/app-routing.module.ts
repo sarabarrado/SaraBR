5300cc3ed714e8153db2a44dc3da486fda46f672
Francisco Javier Bonnet Vicente <javiervicente60@gmail.com>
2023-01-19 18:36:24 +0100

    Router con el login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AutoresComponent } from './components/autores/autores.component';
+import { HomeComponent } from './components/home/home.component';
 import { LibrosComponent } from './components/libros/libros.component';
 import { RegistroUsuarioComponent } from './components/registro-usuario/registro-usuario.component';
 import { UsuariosComponent } from './components/usuarios/usuarios.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [ 
-{ path: '', component: LoginComponent },
-{ path: 'home', component: AppComponent },
+{ path: '', component: AppComponent },
+{ path: 'home', component: HomeComponent },
 { path: 'libros', component: LibrosComponent },
 { path: 'login', component: LoginComponent },
 { path: 'autores', component: AutoresComponent },
